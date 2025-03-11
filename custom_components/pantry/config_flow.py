e54f1d03a3af7190a1b04c44b56854915167d722
Ola Malnes <olamalnes@outlook.com>
2025-03-11 14:45:32 +0100

    asd

--- custom_components/pantry/config_flow.py
+++ custom_components/pantry/config_flow.py
@@ -2,7 +2,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
-class PantryConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
